9359cd207f59236f414b5b4ae09b877cd6e5b2dc
justumen <anthony.eriksen@gmail.com>
2024-01-22 22:40:25 +0100

    Add MaterialUI + Test button

--- app/components/TestRedux.tsx
+++ app/components/TestRedux.tsx
@@ -1,11 +1,22 @@
 "use client"
-import { useGetUsernameValue } from "@/redux/hooks"
+import { useGetUsernameValue, useSetUsernameValue } from "@/redux/hooks"
+import { Button, Typography } from "@mui/material";
+
 
 const TestRedux = () => {
+    const setUsernameValue = useSetUsernameValue();
+
+    const handleChangeUsername = () => {
+        setUsernameValue("newUsername");
+    };
+
     return (
         <div>
-            From component TestRedux = {useGetUsernameValue()}
+            <Typography>
+                From component TestRedux = {useGetUsernameValue()}
+            </Typography>
+            <Button variant="contained" onClick={handleChangeUsername}>Change username to "newUsername"</Button>
         </div>
-    )
+    );
 };
 export default TestRedux;
